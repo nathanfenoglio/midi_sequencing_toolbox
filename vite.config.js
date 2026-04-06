88afec3e1991d4625dc51e31db0ef477bc948454
nathanfenoglio <nathaniel.fenoglio@mavs.uta.edu>
2026-04-05 21:46:59 -0500

    add ngrok website as allowed host in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: "https://subprofessional-eastward-alysia.ngrok-free.dev/",
+    allowedHosts: [
+      "https://subprofessional-eastward-alysia.ngrok-free.dev/",
+    ]
   }
 });
